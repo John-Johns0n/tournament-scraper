d1beb4107c489bb5d7431fb48becc1e53d3178a9
John-Johns0n <84598043+John-Johns0n@users.noreply.github.com>
2024-11-08 15:36:03 -0500

    Implement RiichiOut scraper

--- main.py
+++ main.py
@@ -4,36 +4,51 @@ from typing import Callable
 from scrapers import wrc_scraper, ema_scraper, riichiout_scraper
 
 
-def write_to_csv(filename: str, data: list[list[str, str, int]]) -> None:
+def write_to_csv(filename: str, path: str, data: list[list[str, str, int]]) -> None:
     """
     Writes a tournament's results to a CSV file.
 
     :param filename: Name to give the CSV file
+    :param path: Destination path
     :param data: Results data
     """
 
-    with open(f'./results/{filename}.csv', mode='w', encoding='utf-8', newline='') as results_file:
+    with open(f'{path}/{filename}.csv', mode='w', encoding='utf-8', newline='') as results_file:
         writer = csv.writer(results_file)
         writer.writerows(data)
 
 
-def scrape_tournaments(path: str, scraper: Callable) -> None:
+def scrape_tournaments(data_path: str, destination_path: str, scraper: Callable) -> None:
     """
     Scrapes the web for tournament results and outputs them to a CSV file.
 
+    :param data_path: The path to the CSV file containing data for the tournaments to scrape
+    :param destination_path: Where to save the tournament results
     :param scraper: Scraper function to use (EMA, WRC, etc.)
-    :param path: The path to the CSV file containing data for the tournaments to scrape
     """
 
-    with open(path, mode='r', encoding='utf-8', newline='') as tournaments:
+    with open(data_path, mode='r', encoding='utf-8', newline='') as tournaments:
         csv_reader = csv.DictReader(tournaments, delimiter=',', quotechar='"')
         for row in csv_reader:
             print(f"Scraping results for {row['results_file_name'].removesuffix('_results')}")
             data = scraper(tournament_code=row['tournament_code'], event_id=row['event_id'])
-            write_to_csv(filename=row['results_file_name'], data=data)
+            write_to_csv(filename=row['results_file_name'], path=destination_path, data=data)
             print("Done! Moving to next tournament in queue.\n")
 
 
 if __name__ == '__main__':
-    scrape_tournaments(path='./tournaments/wrc.csv', scraper=wrc_scraper)
-    scrape_tournaments(path='./tournaments/ema.csv', scraper=ema_scraper)
+    scrape_tournaments(
+        data_path='./tournaments/wrc.csv',
+        destination_path='./results/wrc',
+        scraper=wrc_scraper
+    )
+    scrape_tournaments(
+        data_path='./tournaments/ema.csv',
+        destination_path='./results/ema',
+        scraper=ema_scraper
+    )
+    scrape_tournaments(
+        data_path='./tournaments/riichiout.csv',
+        destination_path='results/riichiout',
+        scraper=riichiout_scraper
+    )
--- scrapers.py
+++ scrapers.py
@@ -69,4 +69,25 @@ def riichiout_scraper(tournament_code: str, event_id: str) -> list[list[str, str
     """
 
     url = f'https://riichiout.com/tournaments/{tournament_code}'
-    raise NotImplementedError('Coming soon!')
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    results_table = soup.find('table', class_='rankings')
+    rows = results_table.find_all('tr')
+
+    tournament_results = [['event_id', 'player_id', 'first_name', 'last_name', 'placement', 'score']]
+
+    for row in rows:
+        cells = row.find_all('td')
+        placement = cells[0].text.rstrip('stndrth')
+        full_name = cells[1].find('a').text
+        try:
+            first_name, last_name = full_name.split(' ')
+        except ValueError:  # No last name
+            first_name = full_name
+            last_name = '(last name)'
+        score = cells[2].text.split('/')[0].strip(' +')
+
+        tournament_results.append([event_id, '', first_name, last_name, placement, score])
+
+    return tournament_results
